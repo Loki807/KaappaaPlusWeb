5325c13e9230c2798ec762bf3af6bcb6ad4ef670
Marshake_Fernando <marshakefernando219@gmail.com>
2025-11-25 20:25:09 +0530

    new changes home page cpller theame change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 import {  tenantResolver } from './core/services/tenant-resolver-resolver';
 
 export const routes: Routes = [
-{ path: '', redirectTo: 'home', pathMatch: 'full' },
+{ path: '', redirectTo: 'tenant-dashboard', pathMatch: 'full' },
    { path: 'home', loadComponent: () => import('./Pages/home/home').then(m => m.Home) },
   { path: 'login', loadComponent: () => import('./core/features/login/login').then(m => m.Login) },
   { path: 'change-password', loadComponent: () => import('./core/features/change-password/change-password').then(m => m.ChangePassword) },
@@ -12,8 +12,8 @@ export const routes: Routes = [
    { path: 'tenant-create', loadComponent: () => import('./core/features/SuperAdmin/tenant-create/tenant-create').then(m => m.TenantCreate) },
    { 
   path: 'tenant-details',
-  loadComponent: () => import('./core/features/SuperAdmin/tenant-details/tenant-details').then(m => m.TenantDetails),
-  resolve: { tenants: tenantResolver }},
+  loadComponent: () => import('./core/features/SuperAdmin/tenant-details/tenant-details').then(m => m.TenantDetails)},
+  // resolve: { tenants: tenantResolver } },
 
   {
   path: 'tenant/view/:id',
