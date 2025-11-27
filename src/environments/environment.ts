3bbfc48095d41454d481c3343f73413565025d0a
sajikaran <kanna20021130@gmail.com>
2025-11-27 13:34:43 +0530

    chages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiBase:'https://localhost:7055/api',  // Base API URL
+  apiBase:'https://kaappaan-api-d7gmdnf3csf6ahd9.centralindia-01.azurewebsites.net/api',
 
   // Define the endpoints related to tenants and tenant users
   endpoints: {
